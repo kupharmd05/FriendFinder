834e8e0a5c9eccbe33d9bbffeea4ec706844027e
kupharmd05 <kupharmd05@gmail.com>
2019-02-22 20:52:18 -0600

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friendsArray = [
+    {
+        name: "Bryce",
+        scores: [
+            "3",
+            "3",
+            "4",
+            "4",
+            "5",
+            "4",
+            "3",
+            "3",
+            "4",
+            "3"
+        ]
+
+    },
+    {
+        name: "Jill",
+        scores: [
+            "2",
+            "3",
+            "2",
+            "5",
+            "2",
+            "3",
+            "3",
+            "4",
+            "2",
+            "1"
+        ]
+    }
+];
+
+module.exports = friendsArray;
---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var app = express();
+
+var port = process.env.port || 8080;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(port, function(){
+    console.log("Listening on PORT: "+ port);
+});
