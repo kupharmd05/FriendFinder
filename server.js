73e2c7f4dae36a28895d1c02d22fe1560b702a38
kupharmd05 <kupharmd05@gmail.com>
2019-02-28 20:34:03 -0600

    capitalized PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var app = express();
 
-var port = process.env.port || 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -10,6 +10,6 @@ app.use(express.json());
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-app.listen(port,"0.0.0.0", function(){
-    console.log("Listening on PORT: "+ port);
+app.listen(PORT,"0.0.0.0", function(){
+    console.log("Listening on PORT: "+ PORT);
 });
